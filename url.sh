55b0ae9a3f39d107d8a71e40a8440df35c0e65bb
ubuntu <alotaibi811@gmail.com>
2015-09-26 01:24:09 +0300

    first commit

---
+++ url.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#by: I'm back
+#Under GPL licence
+
+if [ -z $1 ];then
+echo "	usage: $0 <file>";
+exit 1;
+fi
+
+readarray -t source < $1;
+read -p "what you searching about: " search;
+for i in "${source[@]}"; do
+	echo $'\n\n';
+	echo -e "\033[1;31m${i}\033[0m";
+	curl $i | grep -i "$search" > /dev/null && echo -e "\n\n\n \033[1;32m100/100\033[0m\n";
+done

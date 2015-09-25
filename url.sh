f752a11d93539d1fe22fb3977a6b4827145b8789
I'm back <alotaibi811@gmail.com>
2015-09-26 01:36:21 +0300

    Update url.sh

--- url.sh
+++ url.sh
@@ -10,7 +10,6 @@ fi
 readarray -t source < $1;
 read -p "what do you searching about: " search;
 for i in "${source[@]}"; do
-	echo $'\n\n';
-	echo -e "\033[1;31m${i}\033[0m";
+	echo -e "\n\n\033[1;31m${i}\033[0m";
 	curl $i | grep -i "$search" > /dev/null && echo -e "\n\n\n \033[1;32m100/100\033[0m\n";
 done
